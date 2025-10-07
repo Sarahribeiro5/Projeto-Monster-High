fb3d81b751173dbfaebbe01dfa8a4a5ea27834b5
Sarahribeiro5 <sarahribeiro.tds2.senai@gmail.com>
2025-10-07 08:11:35 -0300

    feat: fix footer

--- src/app/Sobre/page.jsx
+++ src/app/Sobre/page.jsx
@@ -36,8 +36,8 @@ export default function Sobre() {
                         </p>
                     </section>
                 </main>
-                <Footer />
             </div>
+            <Footer />
         </div>
     )
 }
